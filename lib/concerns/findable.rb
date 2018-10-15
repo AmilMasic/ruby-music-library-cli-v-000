b9e6f44a4b8ba4d67d5a865fe630de9a60694281
Amil Masic <amil.masic@gmail.com>
2018-10-15 17:32:02 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -21,10 +21,10 @@ module Concerns
   end
 
   def find_or_create_by_name(name)
-    if find_by_name(name)
-       find_by_name(name)
+    if find_by_name(name) == nil
+       create(name)
     else
-      create(name)
+      find_by_name(name)
     end
   end
 
