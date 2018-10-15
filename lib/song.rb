98c90daa2a2552b10233581f53120a9094c41264
Amil Masic <amil.masic@gmail.com>
2018-10-15 18:43:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -71,7 +71,19 @@ class Song
     genre_name = (array[2].chomp(".mp3"))
     genre = Genre.find_or_create_by_name(genre_name)
     new_song = Song.new(title, artist, genre)
-    # new_song.name =
   end
+
+  def self.create_from_filename(file_name)
+    array = file_name.split(" - ")
+    # binding.pry
+    artist_name = array[0]
+    artist = Artist.find_or_create_by_name(artist_name)
+    title = array[1]
+    genre_name = (array[2].chomp(".mp3"))
+    genre = Genre.find_or_create_by_name(genre_name)
+    new_song = Song.new(title, artist, genre)
+  end
+
+
 
 end
