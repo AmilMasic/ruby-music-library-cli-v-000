b59886122fc73d7a58fed4ca9b491c57b3f4f8ad
Amil Masic <amil.masic@gmail.com>
2018-10-15 18:29:53 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,11 +1,10 @@
 require "pry"
 class MusicImporter
+
   attr_accessor :path
-  # @@files = []
+
   def initialize(path)
-    # binding.pry
     @path = path
-    # @@files = []
   end
 
   def files
